09bdaf62531c853b05b054f954719c1822dc0763
Martin Nacimiento <martinnacimiento98@gmail.com>
2020-05-06 22:14:49 -0300

    fibonnaci retorna array y camelcase en congruencia

--- metodos.py
+++ metodos.py
@@ -6,14 +6,14 @@ from utilidades import isPrime
 # genera una sucesion de numeros pseudoaleatorios con el metodo fibonacci
 def fibonacci(v1, v2, a, n):
     i = 0
-    sucesion = str(v1) + str(v2)
+    sucesion = [v1, v2]
     while (i < n):
         if v2 + v1 <= a:
             k = 0
         else:
             k = -1
         v3 = v1 + v2 + k*a
-        sucesion += str(v3)
+        sucesion.append(str(v3))
         v1 = v2
         v2 = v3
         i += 1
@@ -21,7 +21,7 @@ def fibonacci(v1, v2, a, n):
 
 
 #bueno este metodo generara numeros aleatorios entre cero y nueve
-def congruencial_multiplicativo(s, n, a, m):
+def congruencialMultiplicativo(s, n, a, m):
     resultado = []
     resultado.append(str(s))
     i = 0
